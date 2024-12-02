956d345f2cc5d39daa49e1a7ff48bcf2f2c8ac24
samuraikamel <176351360+samuraikamel@users.noreply.github.com>
2024-12-01 19:06:29 -0800

    add inputs and basic home-manager output

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,14 +2,19 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+
+    home-manager = {
+	url = "github:nix-community/home-manager";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, home-manager }: {
+  	homeConfigurations = {
+		"kamel@eocene" = home-manager.lib.homeManagerConfiguration {
+			pkgs = nixpkgs.legacyPackages.x86_64-linux;
+		};
+	};
   };
 }
